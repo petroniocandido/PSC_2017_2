bb35383e8d6b5d1a7baf821e35454810c87a6285
Petrônio Cândido <petronio.candido@gmail.com>
2017-10-24 10:09:28 -0200

    Import inicial dos dados

---
+++ CodigoFonte/microcom_aplicacao/src/br/edu/ifnmg/psc/MicroCom/Aplicacao/Cliente.java
@@ -0,0 +1,100 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.edu.ifnmg.psc.MicroCom.Aplicacao;
+
+import java.util.Date;
+import java.util.Objects;
+
+/**
+ *
+ * @author petronio
+ */
+public class Cliente implements Entidade {
+    private long id;
+    private String nome, cpf;
+    private Date nascimento;
+
+    public Cliente() {
+    }
+    
+    
+
+    @Override
+    public long getId() {
+        return id;
+    }
+
+    @Override
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public Date getNascimento() {
+        return nascimento;
+    }
+
+    public void setNascimento(Date nascimento) {
+        this.nascimento = nascimento;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 97 * hash + (int) (this.id ^ (this.id >>> 32));
+        hash = 97 * hash + Objects.hashCode(this.nome);
+        hash = 97 * hash + Objects.hashCode(this.cpf);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Cliente other = (Cliente) obj;
+        if (this.id != other.id) {
+            return false;
+        }
+        if (!Objects.equals(this.nome, other.nome)) {
+            return false;
+        }
+        if (!Objects.equals(this.cpf, other.cpf)) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return nome;
+    }
+    
+    
+    
+}
---
+++ CodigoFonte/microcom_aplicacao/src/br/edu/ifnmg/psc/MicroCom/Aplicacao/ViolacaoRegraNegocioException.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.edu.ifnmg.psc.MicroCom.Aplicacao;
+
+/**
+ *
+ * @author petronio
+ */
+public class ViolacaoRegraNegocioException extends Exception {
+
+    public ViolacaoRegraNegocioException(String message) {
+        super(message);
+    }
+    
+    
+}
