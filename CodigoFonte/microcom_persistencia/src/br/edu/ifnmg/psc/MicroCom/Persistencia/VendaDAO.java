3cb440080d4147b8015cd2337887f40dd3c1e77d
Petrônio Cândido <petronio.candido@gmail.com>
2017-11-28 14:56:35 -0200

    Venda e VendaItem: entidades, repositório e DAO

--- CodigoFonte/microcom_persistencia/src/br/edu/ifnmg/psc/MicroCom/Persistencia/VendaDAO.java
+++ CodigoFonte/microcom_persistencia/src/br/edu/ifnmg/psc/MicroCom/Persistencia/VendaDAO.java
@@ -9,11 +9,14 @@ import br.edu.ifnmg.psc.MicroCom.Aplicacao.ClienteRepositorio;
 import br.edu.ifnmg.psc.MicroCom.Aplicacao.ProdutoRepositorio;
 import br.edu.ifnmg.psc.MicroCom.Aplicacao.RepositorioBuilder;
 import br.edu.ifnmg.psc.MicroCom.Aplicacao.Venda;
+import br.edu.ifnmg.psc.MicroCom.Aplicacao.VendaItem;
 import br.edu.ifnmg.psc.MicroCom.Aplicacao.VendaRepositorio;
 import java.sql.Date;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -73,6 +76,8 @@ public class VendaDAO extends DAOGenerico<Venda> implements VendaRepositorio {
             obj.setData( new java.util.Date( dados.getDate("data").getTime() ) );
             obj.setValorTotal(dados.getBigDecimal("valortotal"));
             
+            obj.setItens( AbrirItens(obj) );
+            
             return obj;
             
         } catch (SQLException ex) {
@@ -80,6 +85,50 @@ public class VendaDAO extends DAOGenerico<Venda> implements VendaRepositorio {
         }
         return null;
     }
+    
+    private List<VendaItem> AbrirItens(Venda obj){
+        try {
+            // Utilizando a conexão aberta, cria um Statement (comando)
+            PreparedStatement consulta = BD.getConexao().prepareStatement(
+                    "select id, venda, produto, quantidade, valorunitario from vendasitens where venda = ?");
+            
+            // Coloca o parâmetro da consulta (id)
+            consulta.setLong(1, obj.getId());
+            
+            // Executa a consulta select e recebe os dados de retorno
+            ResultSet dados = consulta.executeQuery();
+            
+            List<VendaItem> itens = new ArrayList<>();
+            while(dados.next())
+                itens.add(carregaObjetoItem(obj, dados));
+            
+            return itens;
+            
+        } catch (SQLException ex) {
+            Logger.getLogger(VendaDAO.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+        return null;
+        
+    }
+    
+    private VendaItem carregaObjetoItem(Venda venda, ResultSet dados){
+        try {
+            VendaItem item = new VendaItem();
+            item.setId(dados.getLong("id"));
+            item.setVenda(venda);
+            item.setProduto( produtos.Abrir( dados.getLong("produto")) );
+            item.setQuantidade(dados.getInt("quantidade"));
+            item.setValorUnitario(dados.getBigDecimal("valorunitario"));
+            
+            return item;
+        } catch (SQLException ex) {
+            Logger.getLogger(VendaDAO.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return null;
+    }
+    
+    
 
     @Override
     protected String carregaParametrosBusca(Venda obj) {
